89cad7fee26cbbec4a135bd67fa987571f91ca8a
Nikita P <nikitospogorelyn@gmail.com>
2024-12-17 17:10:32 +0500

    New update

--- myproject/mainapp/urls.py
+++ myproject/mainapp/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from . import views
 
-urlpatterns = {
-    path('', views.index, name='index')
-}
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('index_2/', views.index_2, name='index_2'),
+]
--- myproject/mainapp/views.py
+++ myproject/mainapp/views.py
@@ -6,3 +6,8 @@ from django.shortcuts import render
 def index(request):
     templates = 'mainapp/index.html'
     return render(request, templates)
+
+
+def index_2(request):
+    templates = 'mainapp/index_2.html'
+    return render(request, templates)
