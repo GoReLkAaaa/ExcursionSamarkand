54a8f9554b37214ef3d062878bff4d05a2247aeb
Nikita P <nikitospogorelyn@gmail.com>
2024-12-24 12:27:21 +0500

    Update url admin site and update slider

--- myproject/myproject/urls.py
+++ myproject/myproject/urls.py
@@ -21,7 +21,7 @@ from django.conf.urls.static import static
 
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('admin-secret-adminsyte/', admin.site.urls),
     path('', include('mainapp.urls')),
 ]
 
